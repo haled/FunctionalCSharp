d2d8cb8fd33cf0a321e080d7daee970a27d275d1
Darren Hale <dhale@clearent.com>
2019-06-14 11:10:04 -0500

    Created basic program with try/catch block.

--- Program.cs
+++ Program.cs
@@ -2,11 +2,26 @@
 
 namespace FunctionalCSharp
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int value = 0;
+            Int32.TryParse(args[0], out value);
+
+            decimal result = 0.0m;
+            var message = "Pending";
+            try
+            {
+                result = (decimal) 1 / value;
+                message = "Success";
+            }
+            catch (System.Exception e)
+            {
+                message = "Failure due to: " + e.Message;
+            }
+
+            Console.WriteLine("The result is {0} with a message of {1}", result, message);
         }
     }
 }
